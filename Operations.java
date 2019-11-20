feefdcaab90620e0bec1bd1a03319e35a87b978e
jinl0402 <57887095+jinl0402@users.noreply.github.com>
2019-11-19 22:19:07 -0600

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -6,6 +6,8 @@ public class Operations {
 
 
     private static DataAccessObject dao;
+    static Scanner input = new Scanner(System.in);
+    private static int id = 1009;
 
     public static void chooseOperation(String type, String operation){
         if(type.toLowerCase().equals("water")){
@@ -43,12 +45,60 @@ public class Operations {
         }
 
     }
-    static Scanner input = new Scanner(System.in);
 
 
+    //HumanResource
+    public static HumRes insertHumRes(){
+        HumRes hr = new HumRes();
+
+
+        hr.setHRId(id++);
+        System.out.println("Enter name:");
+        String name = input.nextLine();
+        hr.setHRName(name);
+
+        System.out.println("Enter address:");
+        String addr = input.nextLine();
+        hr.setHRAddrStr(addr);
+
+        System.out.println("Enter phone number:");
+        String pNum = input.nextLine();
+        hr.setHRPhoneNum(pNum);
+
+        System.out.println("Enter latitude:");
+        double la = input.nextDouble();
+        hr.setHRLatitude(la);
+
+        System.out.println("Enter longitude:");
+        double lo = input.nextDouble();
+        hr.setHRLongitude(lo);
+
+        System.out.println("Enter type:");
+        String type = input.next();
+        hr.setHRType(type);
+
+        System.out.println("Enter description:");
+        String des = input.nextLine();
+        hr.setHRDesc(des);
+
+        System.out.println("Enter open hours:");
+        String openHr =  input.nextLine();
+        hr.setHROpenHoursStr(openHr);
+
+        return hr;
+    }
+
+    public static void updateHumRes(){
+
+    }
+
+    public static void deleteHumRes(){
+
+    }
 
     //Water
     public static void insertWater(){
+        HumRes hr = new HumRes();
 
 
 
@@ -66,19 +116,15 @@ public class Operations {
 
     //Food
     public static void insertFood(){
-        Food food = new Food();
-        System.out.println("Enter HRId:");
-        int id = input.nextInt();
-        food.setHRId(id);
+        HumRes hr = insertHumRes();
+        int id = hr.getHRId();
         System.out.println("Enter food type:");
         String Ftype = input.next();
-        food.setFType(Ftype);
         System.out.println("Enter number of food meals available:");
         int FMealsAva = input.nextInt();
-        food.setFMealsAva(FMealsAva);
         System.out.println("Enter food specific description:");
         String Fdesc = input.nextLine();
-        food.setFDec(Fdesc);
+        Food food = new Food(hr.getHRId(),hr.getHRName(), hr.getHRAddrStr(), hr.getHRPhoneNum(), hr.getHRLatitude(), hr.getHRLongitude(), hr.getHRType(), hr.getHRDesc(),hr.getHROpenHoursStr(), Ftype, FMealsAva, Fdesc);
         dao.executeSQLNonQuery("INSERT INTO Food VALUES(" + food.toString() + ")");
     }
 
